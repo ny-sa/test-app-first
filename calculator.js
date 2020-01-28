f021e61a2ce6de852eaf9dfc2f30b388a83c2dba
nysa <msun698@gmail.com>
2020-01-28 21:11:41 +0100

    Finished the Express.js section

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Calculator
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
@@ -12,6 +13,17 @@ app.post('/', (req, res) => {
   let num1 = Number(req.body.num1);
   let num2 = Number(req.body.num2);
   res.send(`You got ${num1+num2}!`);
+})
+
+//BMI Calculator
+app.get('/bmicalculator', (req, res) => {
+  res.sendFile(__dirname + '/bmicalculator/bmiCalculator.html');
+})
+
+app.post('/bmicalculator', (req, res) => {
+  let kg = parseFloat(req.body.w);
+  let cm = parseFloat(req.body.h);
+  res.send(`Your BMI is ${kg/(cm/100)**2}`);
 })
 
 app.listen(1337, () => {
